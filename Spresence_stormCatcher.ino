0773e297c221c4b3f88b6fb2fab71beed157c34f
Laziz Turakulov <4857092+LazaUK@users.noreply.github.com>
2019-01-05 00:58:19 +0000

    Add files via upload
    Initial .ino file

---
+++ Spresence_stormCatcher.ino
@@ -0,0 +1,52 @@
+/*
+     This is a Hackster.io project, demonstrating capabilities of Sony Spresence board
+     to get sensor data on the light intensity and thunder sound, to detect proximity
+     to the storm front
+
+     Last updated on 4th of Jan, 2019
+     by Laziz Turakulov
+
+*/
+
+int analogPin = A0;     // Analog pin on Spresence extension board
+int lightIntensity = 0; // Light intensity values sent by LDR sensor
+
+void setup() {
+
+  pinMode(A0, INPUT);
+  
+  pinMode(LED0, OUTPUT);  // Set output mode for on-boards LEDs
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+
+  digitalWrite(LED0, HIGH); // Switch on all the LEDs in a sequence
+  delay(100);
+  digitalWrite(LED1, HIGH);
+  delay(100);
+  digitalWrite(LED2, HIGH);
+  delay(100);
+  digitalWrite(LED3, HIGH);
+  delay(1000);
+
+  digitalWrite(LED0, LOW);  // Switch off all the LEDs in a sequence
+  delay(100);
+  digitalWrite(LED1, LOW);
+  delay(100);
+  digitalWrite(LED2, LOW);
+  delay(100);
+  digitalWrite(LED3, LOW);
+  
+  Serial.begin(115200); // Set download speed for the Serial Monitor updates
+  Serial.println("Spresence is ready");
+}
+
+void loop() {
+
+  lightIntensity = digitalRead(analogPin); // Get the light intensity measure from LDR sensor
+  Serial.print("Intensity value: ");
+  Serial.println(lightIntensity);         // Print the values on the System Monitor screen
+
+  delay(100);
+
+}
